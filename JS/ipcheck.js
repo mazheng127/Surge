9359d3130272903002732788be73a7448ec56705
gzfynn <92876417+gzfynn@users.noreply.github.com>
2021-11-05 07:05:48 +0800

    Update ipcheck.js

--- JS/ipcheck.js
+++ JS/ipcheck.js
@@ -1,23 +1,23 @@
-/**
-* 感谢@congcong大佬提供的js，原文件地址：https://github.com/congcong0806/surge-list/blob/master/Script/ipcheck.js
-* 用法
+/*
+https://raw.githubusercontent.com/congcong0806/surge-list/master/Script/ipcheck.js
+
 * [Panel]
-* #节点检测
-* ipcheck = script-name=ipcheck, title="节点信息", content="请刷新", style=info, update-interval=1
+* 节点信息 = script-name=节点信息, title="节点信息", content="请刷新", style=info, update-interval=60
 * ...
 * [Script]
-* #节点检测
-* ipcheck = type=generic,timeout=3,script-path=https://raw.githubusercontent.com/gzfynn/script/main/JS/ipcheck.js
+* 节点信息 = type=generic,timeout=3,script-path=https://raw.githubusercontent.com/gzfynn/script/main/JS/ipcheck.js
 */
 
 let url = "http://ip-api.com/json/?lang=zh-CN"
 
 $httpClient.get(url, function(error, response, data){
     let jsonData = JSON.parse(data)
+    let ip = jsonData.query
     let country = jsonData.country
     let emoji = getFlagEmoji(jsonData.countryCode)
     let city = jsonData.city
     let isp = jsonData.isp
+    
   body = {
     title: "节点信息",
     content: `IP位置: ${emoji}${country} - ${city}\n运营商: ${isp}`,
