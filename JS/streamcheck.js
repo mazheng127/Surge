f990091bb10c76956ed67ee662b8ea3a749de5e2
gzfynn <92876417+gzfynn@users.noreply.github.com>
2021-11-04 09:44:33 +0800

    Update streamcheck.js

--- JS/streamcheck.js
+++ JS/streamcheck.js
@@ -1,6 +1,3 @@
-/**
-* 感谢@LucaLin233大佬提供的js
-
 const REQUEST_HEADERS = {
   'User-Agent':
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
