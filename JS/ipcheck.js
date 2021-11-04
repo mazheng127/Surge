bcc6487dc7bb65fc129c9745b0594043486b3df1
gzfynn <92876417+gzfynn@users.noreply.github.com>
2021-11-04 21:13:41 +0800

    Update ipcheck.js

--- JS/ipcheck.js
+++ JS/ipcheck.js
@@ -7,30 +7,4 @@
 * ...
 * [Script]
 * #节点检测
-* ipcheck = type=generic,timeout=3,script-path=https://raw.githubusercontent.com/LucaLin233/Luca_Conf/main/Surge/JS/ipcheck.js
-*/
-
-let url = "http://ip-api.com/json/?lang=zh-CN"
-
-$httpClient.get(url, function(error, response, data){
-    let jsonData = JSON.parse(data)
-    let country = jsonData.country
-    let emoji = getFlagEmoji(jsonData.countryCode)
-    let city = jsonData.city
-    let isp = jsonData.isp
-  body = {
-    title: "节点信息",
-    content: `IP位置: ${emoji}${country} - ${city}\n运营商: ${isp}`,
-    icon: "globe.asia.australia.fill"
-  }
-  $done(body);
-});
-
-
-function getFlagEmoji(countryCode) {
-    const codePoints = countryCode
-      .toUpperCase()
-      .split('')
-      .map(char =>  127397 + char.charCodeAt());
-    return String.fromCodePoint(...codePoints);
-}
+* ipcheck = type=generic,timeout=3,script-path= https://raw.githubusercontent.com/gzfynn/script/main/JS/ipcheck.js
